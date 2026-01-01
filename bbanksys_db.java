3ed340e320ed726a680b907f4359fa538a555be1
vallabhmg <vallabhgorule2005@gmail.com>
2026-01-01 18:23:12 +0530

    A new java File Added by Main_b

---
+++ bbanksys_db.java
@@ -0,0 +1,16 @@
+/*This Code of Mini Project of Banking System(Bank Name = vtm bank),this is a console base(Terminal Base Application to Open Account in Bank) Program.
+#1)Account Class#In this user can open an account on in vtm bank and also Handle the User Account in the Database.It can also manupulate the account
+#2)User#In this user have to register on Website(i.e on vtm Bank)
+#3)Account Manager#In this user can perform Trasanction of money and also Check the  Available Balance
+#4)Banking App#In this,the main Class is Present and handle the all above classes 
+import java.util.Scanner;
+public class bbanksys_db{
+
+
+public static void main(String args){
+
+
+
+
+}
+}
